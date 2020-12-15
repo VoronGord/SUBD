8f42bd51a829e4d2d3a5a017256fb2b8ea1759ba
VoronGord <56074431+VoronGord@users.noreply.github.com>
2020-12-15 11:55:54 +0400

    Update script.cs

--- SUBD/script.cs
+++ SUBD/script.cs
@@ -1,4 +1,4 @@
-﻿using DB.Logic;
+using DB.Logic;
 using DB.Models;
 using System;
 using System.Collections.Generic;
@@ -9,14 +9,12 @@ namespace DB
 {
     class script
     {
-        
-        public double LabScript()
+        public double LabScriptGroup()
         {
-            // вывести число преподавателей по кафедрам
-            var startTime = DateTime.Now;
+                  var startTime = DateTime.Now;
             var clientGroups = OrderLogic
                 .Read(new Order { Id = 0 })
-                .Join(context.Client,
+                .Join(сontext.Client,
                     t => t.clientId,
                     d => d.Id,
                     (t, d) => new
@@ -31,33 +29,32 @@ namespace DB
             {
                 Console.WriteLine(group.Key + " " + group.Date());
             }
-            Console.WriteLine("---Скрипт LabScript выполнен");
+            Console.WriteLine("---Скрипт группировка выполнен");
             return (finishTime - startTime).TotalMilliseconds;
         }
     }
 
-    public double LabScript2()
+    public double LabScriptSershClien()
     {
-        // отобразить клиентов, полученных за последние 100 дней
+        // отобразить клиентов, полученных за последние 30 дней
         var startTime = DateTime.Now;
         var clients = OrderLogic.Read(new Order { Id = 0 }).Join(context.Clients,
                 t => t.clientId,
                 d => d.Id)
-            .Where(rec => rec.Order.Date > DateTime.Now.AddDays(-100))
+            .Where(rec => rec.Order.Date > DateTime.Now.AddDays(-30))
                 .ToList();
         var finishTime = DateTime.Now;
         foreach (var r in client)
         {
             Console.WriteLine($"{r.name} {r.number} ");
         }
-        Console.WriteLine("---Скрипт LabScript2 выполнен");
+        Console.WriteLine("---Скрипт LabScriptSearchClient выполнен");
         return (finishTime - startTime).TotalMilliseconds;
     }
 
-    public double Script3()
+    public double ScriptSerch_forPrice()
     {
-        var product = new Product;
-       
+        var product = new Product() ;       
         var startTime = DateTime.Now;
         var products = ProductLogic.Read(product).Where(rec => rec.Price < 2000);
         var finishTime = DateTime.Now;
@@ -65,7 +62,7 @@ namespace DB
         {
             Console.WriteLine(s.Name + " " + s.Price);
         }
-        Console.WriteLine("---Скрипт scrip3 выполнен");
+        Console.WriteLine("---Скрипт ScriptSerchOrderforPrice выполнен");
         return (finishTime - startTime).TotalMilliseconds;
     }
 }
