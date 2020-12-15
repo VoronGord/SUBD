3eeba951568b4a4794334bd331450ac918d8a817
VoronGord <56074431+VoronGord@users.noreply.github.com>
2020-12-15 11:54:59 +0400

    Update delete.cs

--- SUBD/delete.cs
+++ SUBD/delete.cs
@@ -1,35 +1,34 @@
-﻿using DB.Logic;
+using DB.Logic;
 using DB.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
-
 namespace DB
 {
     class delete
     {
-        public double DeleteClient()
+        public double DeleteClient(string names)
         {
-            var client = new Client { Name = "dmitriy" };
+            var client = new Client { Name = names };
             var startTime = DateTime.Now;
             ClientLogic.Delete(client);
             var finishTime = DateTime.Now;
             Console.WriteLine("---Скрипт Deleteclient выполнен");
             return (finishTime - startTime).TotalMilliseconds;
         }
-        public double DeleteProvider()
+        public double DeleteProvider(int id)
         {
-            var provider = new Provider { Id = 5001 };
+            var provider = new Provider { Id = id };
             var startTime = DateTime.Now;
             ProviderLogic.Delete(provider);
             var finishTime = DateTime.Now;
             Console.WriteLine("---Скрипт Deleteprovider выполнен");
             return (finishTime - startTime).TotalMilliseconds;
         }
-        public double DeleteType()
+        public double DeleteType(string types)
         {
-            var type = new Models.DType { Types = "shots" };
+            var type = new Models.DType { Types = types };
             var startTime = DateTime.Now;
             TypeLogic.Delete(type);
             var finishTime = DateTime.Now;
