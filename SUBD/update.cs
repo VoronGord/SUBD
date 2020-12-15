b5b6412a30736c9a419c9be78708db0bf6fa5abd
VoronGord <56074431+VoronGord@users.noreply.github.com>
2020-12-15 11:56:22 +0400

    Update update.cs

--- SUBD/update.cs
+++ SUBD/update.cs
@@ -1,24 +1,23 @@
-﻿using DB.Logic;
+using DB.Logic;
 using DB.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-
 namespace DB
 { 
     class update 
     {
-        public double UpdateProvider()
+        public double UpdateProvider(string NamePost, string NameNow)
         {
             var providerToUpd = ProviderLogic
-                .Read(new Provider { Name = "Oleg" })
+                .Read(new Provider { Name = NamePost })
                 .FirstOrDefault();
             var newProvider = new Provider
             {
                 Id = providerToUpd.Id,
-                Name = "Adidas",
+                Name = NameNow,
                 Number = providerToUpd.Number
             };
             var startTime = DateTime.Now;
@@ -28,15 +27,15 @@ namespace DB
             return (finishTime - startTime).TotalMilliseconds;
         }
 
-        public double UpdateType()
+        public double UpdateType(string TypePost, string TypeNow)
         {
             var typeToUpd = TypeLogic
-                .Read(new DType { Types = "футболка" })
+                .Read(new DType { Types = TypePost })
                 .FirstOrDefault();
             var newType = new GoryaynovDB.Models.DType
             {
                 Id = typeToUpd.Id,
-                Types = "shots"
+                Types = TypeNow
 
             };
             var startTime = DateTime.Now;
